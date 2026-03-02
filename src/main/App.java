65b4467d6fc50ca095a19212ca9162c273575542
swetha290605 <sevuganswetha@gmail.com>
2026-03-02 21:33:19 +0530

    First Commit of Maven

---
+++ src/main/App.java
@@ -0,0 +1,28 @@
+package com.example;
+
+public class App {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        App calculator = new App();
+        System.out.println("Addition: " + calculator.add(10, 5));
+    }
+}
